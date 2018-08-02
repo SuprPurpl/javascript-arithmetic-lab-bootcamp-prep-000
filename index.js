75d8085109636fad0a2d928bd795df7c8217e5c6
Ethan Murphy <verakai141@gmail.com>
2018-08-02 17:18:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b) {
+var result=(a+b);
+return result;
+}
+
+function subtract(a,b) {
+var result=(a-b);
+return result;
+}
+
+function multiply(a,b) {
+var result=(a*b);
+return result;
+}
+
+function divide(a,b) {
+var result=(a/b);
+return result;
+}
+
+function inc(n) {
+var result=(n+=1);
+return result;
+}
+
+function dec(n) {
+var result=(n-=1);
+return result;
+}
+
+function makeInt(n) {
+parseInt(n,10);
+return n;
+}
